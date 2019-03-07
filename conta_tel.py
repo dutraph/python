004b9cf7d589daa89e225e05b6d751d192e733a3
Paulo Silva <dutra.phs@gmail.com>
2019-03-07 17:49:55 +1100

    Importacao inicial

---
+++ conta_tel.py
@@ -0,0 +1,14 @@
+minutos = int(input("Minutos utilizados: "))
+if minutos < 200:
+    preco = 0.20
+    print ('Tarifa %.2f' %preco)
+elif minutos <= 400:
+    preco = 0.18
+    print ('Tarifa %.2f' %preco)
+elif minutos < 800:
+    preco = 0.15
+    print ('Tarifa %.2f' %preco)
+else:
+    preco = 0.08
+    print ('Tarifca promocional aplicada')
+print ("Conta telefonica: R$ %.2f" % (minutos * preco))
