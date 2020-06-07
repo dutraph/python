2bd36f85b1f4c0b9420b16d4f878ffaa742e9017
Paulo Dutra (Windows) <dutra.phs@gmail.com>
2020-06-07 18:17:13 -0300

    'add'

--- exercicios_feitos/ex05_if.py
+++ exercicios_feitos/ex05_if.py
@@ -10,6 +10,6 @@ while k <= 4:
 if media < 7:
     print(f'Reprovado, sua media eh {media}.')
 elif media == 10:
-    print(f'Aprovado com Distincao, Sua media eh de {media}.')
+    print(f'Aprovado, com Distincao, Sua media eh de {media}.')
 else:
-    print(f'Aprovado sua media eh {media}') 
+    print(f'Aprovado, sua media eh {media}') 
